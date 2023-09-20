5bfb54f69f545cd0439854cdb38e1ce0d776fac4
Vitaly <vitalydude83@gmail.com>
2023-09-20 19:53:18 +0300

    new commit

---
+++ practice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*int q = 6;
+    q > 10 ? printf("%d\n", q) : q < 5 ? printf("%d\n", q*q*q) : printf("%d\n", q*q);
+    return 0;*/
+    int q = 2;
+    for(int i = 0; i <= 15; i++){
+        for(int w = 1; w <= i; w++){
+        q = q*q;
+        printf("%d\n", q*q);
+        }
+    }
+}
+// вывести все степени 2 с 0 до 15
+// определить порядок действий q = q*q;
+//        printf("%d\n", q*q);
+// все в git
