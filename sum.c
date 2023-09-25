2ca03be5b511273313526f33f3880f1bd1b41c98
Vitaly <vitalydude83@gmail.com>
2023-09-25 17:48:49 +0300

    sum homework

--- sum.c
+++ sum.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main ()
 {
@@ -6,5 +6,18 @@ int main ()
         int sum  = sum + i;
         printf("%d\n", sum);
     }
+    return 0;
+}*/
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    
+    for (int i = 1; i <= 1000; i++) {
+        sum += i;
+    }
+    
+    printf("Сумма чисел от 1 до 1000: %d\n", sum);
+    
     return 0;
 }
