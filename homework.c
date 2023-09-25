87e745112ac264adbff4716b7c6a057ec273515a
Vitaly <vitalydude83@gmail.com>
2023-09-25 14:56:20 +0300

    sum homework

--- homework.c
+++ homework.c
@@ -2,6 +2,24 @@
 
 int main ()
 {
+    int q[10];
+    int i = 0;
+    while(i = 1 && i <= 10){
+        q[i] = 0;
+        i++;
+        int result = 1;
+        int e = 0;
+            while(e = 1 && e <= i){
+                result *= i;
+                printf("%d\n", result);
+                e++;
+            }
+    }
+    return 0;
+}
+
+/*int main ()
+{
     for(int i = 1; i <= 15; i++){
         int q = 1;
         for(int w = 1; w <= i; w++){
@@ -10,4 +28,20 @@ int main ()
         printf("%d\n", q);
     }
         return 0;
-}
+}*/
+/* #include <stdio.h>
+
+int main() {
+    int i, j, result;
+    
+    for (i = 1; i <= 10; i++) {
+        result = 1;
+        for (j = 1; j <= i; j++) {
+            result *= i;
+        }
+        printf("%d\n", result);
+    }
+    
+    return 0;
+}*/
+//это вариант chat gpt, мне нравится больше//
