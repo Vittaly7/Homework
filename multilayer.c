e0a61da64749b78e6fe5016fece0788a3e855d97
Vitaly <vitalydude83@gmail.com>
2023-10-08 17:16:20 +0300

    new commit

--- multilayer.c
+++ multilayer.c
@@ -3,20 +3,14 @@
 int main()
 {
 int big = 3888;
-    for(int i = 1; i <= 3888; i++){
-        if(big % i == 0)
-        for(int q = 1; q <= i; q += 2){
-            if(i % q == 0){
-                if(i < 62){
-                    printf("{%d} - %d", i, q);
-                    printf("\n");
-                }
-                else{
-                    printf("%d - %d", i, q);
-                    printf("\n");
-                }
-            }
+    for(int i = 1; i <= big; i++){
+        if(big % i == 0);
+        i <= 62? printf("{%d} - ", i):printf(" %d", i);
+            for(int q = 1; q <= i; q++){
+                if(i % q == 0 && q % 2 != 0)
+                q % 9 == 0? printf(" [%d]", q):printf(" %d", q);
         }
+        printf("\n");
     }
 return 0;
 }
